29d9521d443e9b3c6a2cca71a47ef4b2812da65b
Takue12 <bmandizvidza@mail.pima.edu>
2025-01-31 22:05:56 -0700

    Delete cis129_lab02_shortStory.py

--- cis129_lab02_shortStory.py
+++
@@ -1,7 +0,0 @@
-#Author : Bashir Satoro
-#A simple python program that prints a short story
-print("As the sun goes down Milo and his dog Bruno decided to go deep in the forest to hunt.")
-print("On their way to the forest Milo started singing for his dog to cheer him up for their hunting adventure.")
-print("Milo saw a big deer and slowly he took out his hunting gun to shoot the deer unfortunatley due to his bad eye sight he missed the deer and it ran away because of the firing sound.")
-print("Bruno was enegized by the speed of the deer he chased the deer all the way to the  river where it was stuck because of the crocodiles in the river.")
-print("Milo quickly took out his gun again as the deer was runnning being chased by Bruno and he shot the deer close to the neck this time he made a perfect shot despite the range he was and they took the deer home  for a feast.")
